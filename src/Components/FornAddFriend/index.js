eb290ba5bf8f90f1940762eb92b276f806a2bbda
SaeedAbi <abdary.saeed@gmail.com>
2024-05-07 15:19:02 +0330

    :building_construction: add friend form component

---
+++ src/Components/FornAddFriend/index.js
@@ -0,0 +1,9 @@
+
+const FormAddFriend=()=>{
+    return <form className="form-add-friend">
+        <label>friend name</label>
+        <input type="text"/>
+        <label >Image URL</label>
+        <input type="text"/>
+    </form>
+}
