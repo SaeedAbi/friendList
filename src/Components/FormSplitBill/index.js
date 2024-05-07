c467814906d1152ea801db0577c5669b2e0289a7
SaeedAbi <abdary.saeed@gmail.com>
2024-05-07 15:38:23 +0330

    :building_construction:update static split form component

--- src/Components/FormSplitBill/index.js
+++ src/Components/FormSplitBill/index.js
@@ -1,6 +1,23 @@
+import Button from "../Button";
+
 const FormSplitBill=()=>{
     return <form className="form-split-bill">
         <h2>Split a bill with X</h2>
+        <label>Bill value</label>
+        <input type="text"/>
+
+        <label>your expense</label>
+        <input type="text"/>
+        <label>x expense</label>
+        <input type="text" disabled/>
+
+        <label>who is paying the bill?</label>
+        <select>
+            <option value="user">You</option>
+            <option value="friend">X</option>
+        </select>
+
+        <Button>Split bill</Button>
     </form>
 }
 export default FormSplitBill
