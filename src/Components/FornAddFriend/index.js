c401a0fb8f7e78999909697b5542e8f934f6c2f5
SaeedAbi <abdary.saeed@gmail.com>
2024-05-07 20:17:22 +0330

    :zap: :building_construction:make FormAddFriend component to controlled element

--- src/Components/FornAddFriend/index.js
+++ src/Components/FornAddFriend/index.js
@@ -1,11 +1,14 @@
 import Button from "../Button";
+import {useState} from "react";
 
 const FormAddFriend=()=>{
+    const [name,setName]=useState("")
+    const [image,setImage]=useState("")
     return <form className="form-add-friend">
         <label>friend name</label>
-        <input type="text"/>
+        <input type="text" value='name' onChange={(e)=>setName(e.target.value)}/>
         <label >Image URL</label>
-        <input type="text"/>
+        <input type="text" value='image' onChange={(e)=>{setImage(e.target.value)}}/>
         <Button>Add</Button>
     </form>
 }
